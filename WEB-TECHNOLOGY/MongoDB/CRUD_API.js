e81a65050861b7cef54e959606200c323ff91a8e
keval2310 <142521291+keval2310@users.noreply.github.com>
2024-09-29 17:17:47 +0530

    Add files via upload

---
+++ WEB-TECHNOLOGY/MongoDB/CRUD_API.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const faculty = require('./Model/Faculty');
+
+
+//Input your connection string
+const connectionString = '';
+
+mongoose.connect(connectionString).then(()=>{
+
+    const app = express();
+
+    app.use(bodyParser.urlencoded({extended: false}));
+
+    // app.use(express.json());
+
+    console.log("Connection Established");
+
+    //GET ALL
+    app.get('/home', async(req, res)=>{
+        const result = await student.find();
+        res.json(result);
+    });
+
+    //GET BY ID
+    app.get('/home/:id', async(req, res)=>{
+        const result = await faculty.findOne({id: req.params.id});
+        res.json(result);
+    });
+
+    //INSERT
+    app.post('/add', async(req, res)=>{
+        const data = new faculty(req.body);
+        const result = await data.save();
+        res.json(result);
+    });
+
+    //UPDATE
+    app.patch('/update/:id', async(req, res)=>{
+        const result = await faculty.findOneAndUpdate({id: req.params.id}, req.body, {new: true});
+        res.json(result); 
+    });
+
+    //DELETE
+    app.delete('/home/delete/:id', async(req, res)=>{
+        const result = await faculty.deleteOne({id: req.params.id});
+        res.json(result);
+    }
+               
+    app.listen(7000, ()=>{
+        console.log("Server initialised...");
+    });
+
+});
